dd8bae87c20192ea882b48ca1f4598fe887391fa
Raphaël Catarino <raphcatarino@gmail.com>
2023-04-06 10:32:41 +0200

    added bitwarden function

--- macos/.zsh/macos.zsh
+++ macos/.zsh/macos.zsh
@@ -7,3 +7,10 @@ eval "$(fnm env --use-on-cd)"
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias batcat="bat"
 alias dup="docker compose up -d"
+alias bwsession="bwses"
+alias bwunlock="bwses"
+
+function bwses(){
+    BW_SESSION=$(bw unlock --raw)
+    export BW_SESSION
+}
