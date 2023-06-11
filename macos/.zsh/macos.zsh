fe1aff987b14defef453793884a12854fdcc693f
Raphaël Catarino <raphcatarino@gmail.com>
2023-06-11 12:56:05 +0200

    bun completions

--- macos/.zsh/macos.zsh
+++ macos/.zsh/macos.zsh
@@ -4,13 +4,16 @@ eval "$(pyenv init -)"
 # Load fnm
 eval "$(fnm env --use-on-cd)"
 
+# Bun completions
+[ -s "/Users/raphaelgc/.bun/_bun" ] && source "/Users/raphaelgc/.bun/_bun"
+
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias batcat="bat"
 alias dup="docker compose up -d"
 alias bwsession="bwses"
 alias bwunlock="bwses"
 
-function bwses(){
+function bwses() {
     BW_SESSION=$(bw unlock --raw)
     export BW_SESSION
 }
