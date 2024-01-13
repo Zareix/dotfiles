3ab51c0f4df22e4daf778600fe1f23b3537e3db5
Raphaël Catarino <raphcatarino@gmail.com>
2024-01-13 16:09:19 +0100

    Add source command for dietpi.bash and update shutdown alias

--- zsh/.zsh/rpi.zsh
+++ zsh/.zsh/rpi.zsh
@@ -1,2 +1,4 @@
+[[ -f /etc/bashrc.d/dietpi.bash ]] && source /etc/bashrc.d/dietpi.bash
+
 alias shutdown="echo 'Please do not use this command. Use the command xoff instead.'"
 alias xoff='sudo /usr/local/bin/x-c1-softsd.sh'
