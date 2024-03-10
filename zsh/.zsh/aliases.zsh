d6913c7de6f862f9de65923da000a336d4429974
Raphaël Catarino <raphcatarino@gmail.com>
2024-03-10 21:14:04 +0100

    docker aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -15,8 +15,38 @@ alias tree="lt"
 alias cat="batcat --theme=OneHalfDark"
 alias py="python"
 alias tf="terraform"
+alias d="docker"
+alias dc="docker compose"
 alias dup="docker compose up -d"
-alias dup-dev="docker compose -f docker-compose.dev.yml up -d"
+# alias dup-dev="docker compose -f docker-compose.dev.yml up -d"
+
+function dc-dev() {
+    if [ -f "docker-compose.dev.yml" ]; then
+        docker compose -f docker-compose.dev.yml "$@"
+    elif [ -f "docker-compose.dev.yaml" ]; then
+        docker compose -f docker-compose.dev.yaml "$@"
+    elif [ -f "compose.dev.yaml" ]; then
+        docker compose -f compose.dev.yaml "$@"
+    elif [ -f "compose.dev.yml" ]; then
+        docker compose -f compose.dev.yml "$@"
+    else
+        echo "No docker-compose.dev.yml / docker-compose.dev.yaml / compose.dev.yaml / compose.dev.yml found"
+    fi
+}
+
+function dup-dev() {
+    if [ -f "docker-compose.dev.yml" ]; then
+        docker compose -f docker-compose.dev.yml up -d
+    elif [ -f "docker-compose.dev.yaml" ]; then
+        docker compose -f docker-compose.dev.yaml up -d
+    elif [ -f "compose.dev.yaml" ]; then
+        docker compose -f compose.dev.yaml up -d
+    elif [ -f "compose.dev.yml" ]; then
+        docker compose -f compose.dev.yml up -d
+    else
+        echo "No docker-compose.dev.yml / docker-compose.dev.yaml / compose.dev.yaml / compose.dev.yml found"
+    fi
+}
 
 if [ "$EUID" -eq 0 ]; then
     alias sudo=""
