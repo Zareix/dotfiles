0fdc7aa30047481d5293fdafc891785c33a8b124
Raphaël Catarino <raphcatarino@gmail.com>
2024-04-30 01:09:07 +0200

    Revert "stow"
    This reverts commit a1b33c454f72dba99a1f360fc15deee1df64d510.

--- stow-linux.sh
+++ stow-linux.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-#stow --adopt git nano fastfetch starship zsh linux
-r=$(realpath $HOME/dotfiles) && target=${r%/*} || echo "Error!!"
-stow --adopt git nano fastfetch starship zsh linux -t $target
+stow --adopt git nano fastfetch starship zsh linux
