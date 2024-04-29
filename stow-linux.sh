a1b33c454f72dba99a1f360fc15deee1df64d510
Raphaël Catarino <raphcatarino@gmail.com>
2024-04-30 01:01:14 +0200

    stow

--- stow-linux.sh
+++ stow-linux.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-stow --adopt git nano fastfetch starship zsh linux
+#stow --adopt git nano fastfetch starship zsh linux
+r=$(realpath $HOME/dotfiles) && target=${r%/*} || echo "Error!!"
+stow --adopt git nano fastfetch starship zsh linux -t $target
