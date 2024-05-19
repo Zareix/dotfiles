837250796846813c79828a3ef75b89329f614b76
Raphaël Catarino <raphcatarino@gmail.com>
2024-05-19 23:05:21 +0200

    fix distro files

--- zsh/.zsh/distro.zsh
+++ zsh/.zsh/distro.zsh
@@ -1,6 +1,5 @@
-LFILE="/etc/*-release"
 MFILE="/System/Library/CoreServices/SystemVersion.plist"
-if [[ -f $LFILE ]]; then
+if [[ -f /etc/os-release || -f /etc/lsb-release ]]; then
   _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 elif [[ -f $MFILE ]]; then
   _distro="macos"
